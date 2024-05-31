ee536e84c3ef2ca17c8d398e0e7c08ecc5f0a25f
duckietm <troop@mailtjes.com>
2024-05-31 11:03:22 +0200

    :up: Fix the GameCenter Queue

--- src/components/gamecenter/GameCenterQueue.tsx
+++ src/components/gamecenter/GameCenterQueue.tsx
@@ -1,7 +1,7 @@
 import { ILinkEventTracker } from '@nitrots/nitro-renderer';
 import { FC, useEffect, useState } from 'react';
 import useWebSocket from 'react-use-websocket';
-import { AddEventLinkTracker, CreateLinkEvent, GetConfiguration, RemoveLinkEventTracker } from '../../api';
+import { AddEventLinkTracker, CreateLinkEvent, GetConfiguration, LocalizeText, RemoveLinkEventTracker } from '../../api';
 import { NitroCardContentView, NitroCardHeaderView, NitroCardView } from '../../common';
 import { useSessionInfo } from '../../hooks';
 import { useIsPlaying } from '../../hooks/game-center';
@@ -104,134 +104,35 @@ export const GameCenterQueue: FC<{}> = props =>
     }, [ setIsVisible ]);
 
     return (
-        <>
-            { isVisible &&
-                <NitroCardView style={{width: "500px"}}>
-                    <NitroCardHeaderView headerText="Centro de juegos"  onCloseClick={ event => setIsVisible(false) }/>
-                    <NitroCardContentView>
-                        <h5 className='text-center text-dark mt-2'><b>Espera a otros jugadores <img src="https://2.bp.blogspot.com/-P6oQWRcF-Ug/V1kVsrAPbxI/AAAAAAAAqAU/96iLr_IVF0Y7vojQsyGSMB22aOWSZ_QhgCKgB/s1600/progressbubbles.gif" /></b></h5>
-                        <div className='row'>
-
-                            {players[0] != null ?
-                                <GameCenterQueuePlayer figure={players[0].figure} name={players[0].username} />
-                                :
-                                <div className='col-md-2 mt-2' style={{cursor: "pointer"}}>
-                                    <div className='text-center' style={{backgroundColor: "rgba(189, 200, 208, 255)", padding: "32px", borderRadius: "10px"}}>
-                                        <img style={{position: "absolute", marginLeft: "-16px", marginTop: "-23px"}} src="https://1.bp.blogspot.com/-pmAeY9LK0_I/X7MoUyBaEeI/AAAAAAABfRk/UI7s-1orVpYSszigbkaRNd7eVUjKn-qtACPcBGAsYHg/s0/26_HabboAvatarRenderLib_h_std_ha_15_2_0.png" />
+    <>
+        {isVisible && 
+            <NitroCardView style={{width: "500px"}}>
+                <NitroCardHeaderView headerText={LocalizeText('gamecenter.waiting.game.title')} onCloseClick={event => setIsVisible(false)} />
+                <NitroCardContentView>
+                    <h5 className="waiting-header">
+                        <b>{LocalizeText('snowwar.waiting_players')} <span className="waiting-img"></span></b>
+                    </h5>
+                    <div className='row'>
+                        {[...Array(12).keys()].map(index => (
+                            <div key={index} className='col-md-2 mt-2' style={{cursor: "pointer"}}>
+                                {players[index] ? (
+                                    <div className="avatar-position">
+                                        <GameCenterQueuePlayer figure={players[index].figure} name={players[index].username} />
                                     </div>
-                                </div>
-                            }
-
-                            {players[1] != null ?
-                                <GameCenterQueuePlayer figure={players[1].figure} name={players[1].username} />
-                                :
-                                <div className='col-md-2 mt-2' style={{cursor: "pointer"}}>
-                                    <div className='text-center' style={{backgroundColor: "rgba(189, 200, 208, 255)", padding: "32px", borderRadius: "10px"}}>
-                                        <img style={{position: "absolute", marginLeft: "-16px", marginTop: "-23px"}} src="https://1.bp.blogspot.com/-pmAeY9LK0_I/X7MoUyBaEeI/AAAAAAABfRk/UI7s-1orVpYSszigbkaRNd7eVUjKn-qtACPcBGAsYHg/s0/26_HabboAvatarRenderLib_h_std_ha_15_2_0.png" />
+                                ) : (
+                                    <div className="avatar-waiting">
+                                        <img className="avatar-image" />
                                     </div>
-                                </div>
-                            }
-
-                            {players[2] != null ?
-                                <GameCenterQueuePlayer figure={players[2].figure} name={players[2].username} />
-                                :
-                                <div className='col-md-2 mt-2' style={{cursor: "pointer"}}>
-                                    <div className='text-center' style={{backgroundColor: "rgba(189, 200, 208, 255)", padding: "32px", borderRadius: "10px"}}>
-                                        <img style={{position: "absolute", marginLeft: "-16px", marginTop: "-23px"}} src="https://1.bp.blogspot.com/-pmAeY9LK0_I/X7MoUyBaEeI/AAAAAAABfRk/UI7s-1orVpYSszigbkaRNd7eVUjKn-qtACPcBGAsYHg/s0/26_HabboAvatarRenderLib_h_std_ha_15_2_0.png" />
-                                    </div>
-                                </div>
-                            }
-                            {players[3] != null ?
-                                <GameCenterQueuePlayer figure={players[3].figure} name={players[3].username} />
-                                :
-                                <div className='col-md-2 mt-2' style={{cursor: "pointer"}}>
-                                    <div className='text-center' style={{backgroundColor: "rgba(189, 200, 208, 255)", padding: "32px", borderRadius: "10px"}}>
-                                        <img style={{position: "absolute", marginLeft: "-16px", marginTop: "-23px"}} src="https://1.bp.blogspot.com/-pmAeY9LK0_I/X7MoUyBaEeI/AAAAAAABfRk/UI7s-1orVpYSszigbkaRNd7eVUjKn-qtACPcBGAsYHg/s0/26_HabboAvatarRenderLib_h_std_ha_15_2_0.png" />
-                                    </div>
-                                </div>
-                            }
-                            {players[4] != null ?
-                                <GameCenterQueuePlayer figure={players[4].figure} name={players[4].username} />
-                                :
-                                <div className='col-md-2 mt-2' style={{cursor: "pointer"}}>
-                                    <div className='text-center' style={{backgroundColor: "rgba(189, 200, 208, 255)", padding: "32px", borderRadius: "10px"}}>
-                                        <img style={{position: "absolute", marginLeft: "-16px", marginTop: "-23px"}} src="https://1.bp.blogspot.com/-pmAeY9LK0_I/X7MoUyBaEeI/AAAAAAABfRk/UI7s-1orVpYSszigbkaRNd7eVUjKn-qtACPcBGAsYHg/s0/26_HabboAvatarRenderLib_h_std_ha_15_2_0.png" />
-                                    </div>
-                                </div>
-                            }
-
-                            {players[5] != null ?
-                                <GameCenterQueuePlayer figure={players[5].figure} name={players[5].username} />
-                                :
-                                <div className='col-md-2 mt-2' style={{cursor: "pointer"}}>
-                                    <div className='text-center' style={{backgroundColor: "rgba(189, 200, 208, 255)", padding: "32px", borderRadius: "10px"}}>
-                                        <img style={{position: "absolute", marginLeft: "-16px", marginTop: "-23px"}} src="https://1.bp.blogspot.com/-pmAeY9LK0_I/X7MoUyBaEeI/AAAAAAABfRk/UI7s-1orVpYSszigbkaRNd7eVUjKn-qtACPcBGAsYHg/s0/26_HabboAvatarRenderLib_h_std_ha_15_2_0.png" />
-                                    </div>
-                                </div>
-                            }
-                            {players[6] != null ?
-                                <GameCenterQueuePlayer figure={players[6].figure} name={players[6].username} />
-                                :
-                                <div className='col-md-2 mt-2' style={{cursor: "pointer"}}>
-                                    <div className='text-center' style={{backgroundColor: "rgba(189, 200, 208, 255)", padding: "32px", borderRadius: "10px"}}>
-                                        <img style={{position: "absolute", marginLeft: "-16px", marginTop: "-23px"}} src="https://1.bp.blogspot.com/-pmAeY9LK0_I/X7MoUyBaEeI/AAAAAAABfRk/UI7s-1orVpYSszigbkaRNd7eVUjKn-qtACPcBGAsYHg/s0/26_HabboAvatarRenderLib_h_std_ha_15_2_0.png" />
-                                    </div>
-                                </div>
-                            }
-                            {players[7] != null ?
-                                <GameCenterQueuePlayer figure={players[7].figure} name={players[7].username} />
-                                :
-                                <div className='col-md-2 mt-2' style={{cursor: "pointer"}}>
-                                    <div className='text-center' style={{backgroundColor: "rgba(189, 200, 208, 255)", padding: "32px", borderRadius: "10px"}}>
-                                        <img style={{position: "absolute", marginLeft: "-16px", marginTop: "-23px"}} src="https://1.bp.blogspot.com/-pmAeY9LK0_I/X7MoUyBaEeI/AAAAAAABfRk/UI7s-1orVpYSszigbkaRNd7eVUjKn-qtACPcBGAsYHg/s0/26_HabboAvatarRenderLib_h_std_ha_15_2_0.png" />
-                                    </div>
-                                </div>
-                            }
-                            {players[8] != null ?
-                                <GameCenterQueuePlayer figure={players[8].figure} name={players[8].username} />
-                                :
-                                <div className='col-md-2 mt-2' style={{cursor: "pointer"}}>
-                                    <div className='text-center' style={{backgroundColor: "rgba(189, 200, 208, 255)", padding: "32px", borderRadius: "10px"}}>
-                                        <img style={{position: "absolute", marginLeft: "-16px", marginTop: "-23px"}} src="https://1.bp.blogspot.com/-pmAeY9LK0_I/X7MoUyBaEeI/AAAAAAABfRk/UI7s-1orVpYSszigbkaRNd7eVUjKn-qtACPcBGAsYHg/s0/26_HabboAvatarRenderLib_h_std_ha_15_2_0.png" />
-                                    </div>
-                                </div>
-                            }
-                            {players[9] != null ?
-                                <GameCenterQueuePlayer figure={players[9].figure} name={players[9].username} />
-                                :
-                                <div className='col-md-2 mt-2' style={{cursor: "pointer"}}>
-                                    <div className='text-center' style={{backgroundColor: "rgba(189, 200, 208, 255)", padding: "32px", borderRadius: "10px"}}>
-                                        <img style={{position: "absolute", marginLeft: "-16px", marginTop: "-23px"}} src="https://1.bp.blogspot.com/-pmAeY9LK0_I/X7MoUyBaEeI/AAAAAAABfRk/UI7s-1orVpYSszigbkaRNd7eVUjKn-qtACPcBGAsYHg/s0/26_HabboAvatarRenderLib_h_std_ha_15_2_0.png" />
-                                    </div>
-                                </div>
-                            }
-                            {players[10] != null ?
-                                <GameCenterQueuePlayer figure={players[10].figure} name={players[10].username} />
-                                :
-                                <div className='col-md-2 mt-2' style={{cursor: "pointer"}}>
-                                    <div className='text-center' style={{backgroundColor: "rgba(189, 200, 208, 255)", padding: "32px", borderRadius: "10px"}}>
-                                        <img style={{position: "absolute", marginLeft: "-16px", marginTop: "-23px"}} src="https://1.bp.blogspot.com/-pmAeY9LK0_I/X7MoUyBaEeI/AAAAAAABfRk/UI7s-1orVpYSszigbkaRNd7eVUjKn-qtACPcBGAsYHg/s0/26_HabboAvatarRenderLib_h_std_ha_15_2_0.png" />
-                                    </div>
-                                </div>
-                            }
-                            {players[11] != null ?
-                                <GameCenterQueuePlayer figure={players[11].figure} name={players[11].username} />
-                                :
-                                <div className='col-md-2 mt-2' style={{cursor: "pointer"}}>
-                                    <div className='text-center' style={{backgroundColor: "rgba(189, 200, 208, 255)", padding: "32px", borderRadius: "10px"}}>
-                                        <img style={{position: "absolute", marginLeft: "-16px", marginTop: "-23px"}} src="https://1.bp.blogspot.com/-pmAeY9LK0_I/X7MoUyBaEeI/AAAAAAABfRk/UI7s-1orVpYSszigbkaRNd7eVUjKn-qtACPcBGAsYHg/s0/26_HabboAvatarRenderLib_h_std_ha_15_2_0.png" />
-                                    </div>
-                                </div>
-                            }
-
-
-                            <div className="col-md-12">
-                            <button onClick={() => cancelMatch()} className='mt-3 btn btn-danger w-100'>Cancel match</button>
+                                )}
                             </div>
+                        ))}
+                        <div className="col-md-12">
+                            <button onClick={() => cancelMatch()} className='mt-3 btn btn-danger w-100'>Cancel match</button>
                         </div>
-                    </NitroCardContentView>
-                </NitroCardView>
-            }
-        </>
-    );
+                    </div>
+                </NitroCardContentView>
+            </NitroCardView>
+        }
+    </>
+);
 }
