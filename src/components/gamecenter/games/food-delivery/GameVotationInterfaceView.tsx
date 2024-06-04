6991d85dc5876c793f2007bc722ce80d86c3c54e
duckietm <troop@mailtjes.com>
2024-06-04 15:52:17 +0200

    :up: Final Battle Build is working

--- src/components/gamecenter/games/food-delivery/GameVotationInterfaceView.tsx
+++ src/components/gamecenter/games/food-delivery/GameVotationInterfaceView.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
-import { GetSessionDataManager } from '../../../../api';
+import { GetSessionDataManager, LocalizeText } from '../../../../api';
 import { LayoutAvatarImageView, NitroCardContentView, NitroCardHeaderView, NitroCardView, Text } from '../../../../common';
 
 interface GameVotationInterfaceProps {
     username: string;
     figure: string;
-    id: number; // ID of the participant being voted on
+    id: number;
     vote: (participantId: number, votationNumber: number) => void;
 }
 
@@ -24,48 +24,41 @@ const VoteButton = ({ label, color, onClick, disabled }: VoteButtonProps) => (
 );
 
 export const GameVotationInterfaceView = ({ username, figure, id, vote }: GameVotationInterfaceProps) => {
-    const [isVisible, setIsVisible] = useState(true); // Default visibility to true
+    const [isVisible, setIsVisible] = useState(true);
     const [hasVoted, setHasVoted] = useState(false);
-    const [isVoting, setIsVoting] = useState(false); // State to handle voting status
+    const [isVoting, setIsVoting] = useState(false);
     const userInfo = GetSessionDataManager().userId;
 
     useEffect(() => {
         if (!hasVoted) {
             setIsVisible(true);
         } else {
-            setIsVisible(false); // Hide the interface if the user has voted
+            setIsVisible(false);
         }
     }, [hasVoted]);
 
-    useEffect(() => {
-        if (userInfo) {
-            console.log(`Look : Current user ID: ${userInfo}`);
-            console.log(`Look: Participant ID: ${id}`);
-        }
-    }, [userInfo, id]);
-
     const handleVote = (votationNumber: number) => {
-        if (!hasVoted && !isVoting) { // Check if the user has already voted or if a vote is in progress
-            setIsVoting(true); // Set voting in progress
-            vote(id, votationNumber); // Ensure this sends the correct participantId
+        if (!hasVoted && !isVoting) {
+            setIsVoting(true);
+            vote(id, votationNumber);
             setHasVoted(true);
-            setIsVoting(false); // Reset voting status
+            setIsVoting(false);
         }
     };
 
     const voteOptions = [
-        { label: 'I Love it', color: '#e163e6', value: 5 },
-        { label: 'Very good', color: '#63e674', value: 4 },
-        { label: 'OK', color: '#63c7e6', value: 3 },
-        { label: 'Have seen better', color: '#c7a52a', value: 2 },
-        { label: 'Very bad', color: '#c72a2a', value: 1 },
+        { label: LocalizeText('gamecenter.vote.room.bestihaveseen'), color: '#e163e6', value: 5 },
+        { label: LocalizeText('gamecenter.vote.room.nice'), color: '#63e674', value: 4 },
+        { label: LocalizeText('gamecenter.vote.room.normal'), color: '#63c7e6', value: 3 },
+        { label: LocalizeText('gamecenter.vote.room.couldbebetter'), color: '#c7a52a', value: 2 },
+        { label: LocalizeText('gamecenter.vote.room.bad'), color: '#c72a2a', value: 1 },
     ];
 
     return (
         <>
             {isVisible && (
                 <NitroCardView style={{ width: "400px", height: "330px" }}>
-                    <NitroCardHeaderView headerText="Poll" onCloseClick={() => setIsVisible(false)} />
+                    <NitroCardHeaderView headerText={LocalizeText('gamecenter.vote.description')} onCloseClick={() => setIsVisible(false)} />
                     <NitroCardContentView>
                         <div className='alert bg-dark text-white mb-0'>
                             <Row>
@@ -73,7 +66,7 @@ export const GameVotationInterfaceView = ({ username, figure, id, vote }: GameVo
                                     <LayoutAvatarImageView style={{ position: "absolute", marginLeft: "-28px", marginTop: "-38px" }} figure={figure} headOnly={true} direction={2} />
                                 </Col>
                                 <Col md={9}>
-                                    <Text variant="white" bold>Space created by {username}</Text>
+                                    <Text variant="white" bold>{LocalizeText('gamecenter.vote.room.made.by')} {username}</Text>
                                 </Col>
                             </Row>
                         </div>
@@ -89,7 +82,7 @@ export const GameVotationInterfaceView = ({ username, figure, id, vote }: GameVo
                             ))
                         ) : (
                             <Text variant="black" className="text-center mt-3">
-                                Please wait until other players have voted
+                                {LocalizeText('gamecenter.vote.room.wait')}
                             </Text>
                         )}
                     </NitroCardContentView>
